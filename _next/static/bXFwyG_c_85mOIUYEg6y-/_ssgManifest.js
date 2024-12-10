a5bc757a2d4230e926a4261dfe20c403cc14d6ea
harry.sitohang <harry.sitohang@prakerja.go.id>
2024-12-10 13:25:04 +0700

    fix code example url from gitlab to github

---
+++ _next/static/bXFwyG_c_85mOIUYEg6y-/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fabout\u002F[slug]","\u002Farticles\u002F[slug]","\u002Farticles\u002Fpage\u002F[page]","\u002Fprojects\u002F[slug]","\u002Fprojects\u002Fpage\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
